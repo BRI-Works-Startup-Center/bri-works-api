b8bcbbe199729ee9b3472d47733febc615ff5939
Achmad Hafiz <achmadhafiz99@gmail.com>
2024-08-17 22:27:00 +0700

    feat: login API and register API

---
+++ authentication/views.py
@@ -0,0 +1,71 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from .serializers import RegisterSerializer, LoginSerializer
+from .models import CustomUser
+
+class RegisterResponse(serializers.Serializer):
+    token = serializers.CharField()
+
+class RegisterAPI(APIView):
+    def post(self, request):
+        register_data = RegisterSerializer(data=request.data)
+        register_data.is_valid(raise_exception=True)
+        register_data = register_data.data
+        new_user = CustomUser.objects.create_user(
+            email=register_data['email'],
+            phone_number=register_data['phone_number'],
+            password=register_data['password']
+            )
+        
+        new_user.save()
+
+        token = Token.objects.create(user=new_user)
+
+        serializer = RegisterResponse(
+            data = {
+                'token': token.key
+            }
+        )
+        serializer.is_valid()
+
+        return Response(serializer.data)
+    
+
+    
+class LoginResponse(serializers.Serializer):
+    message = serializers.CharField()
+    
+class LoginAPI(APIView):
+    def post(self, request):
+        login_data = LoginSerializer(data=request.data)
+        login_data.is_valid(raise_exception=True)
+        login_data = login_data.data
+
+        user = authenticate(
+            request,
+            username=login_data['email'],
+            password=login_data['password']
+            )
+        if user is not None:
+            token = Token.objects.get(user=user)
+            serializer = RegisterResponse(
+                data={
+                    'token': token.key
+                }
+            )
+            serializer.is_valid()
+
+            return Response(serializer.data)
+        else:
+            serializer = LoginResponse(
+                data={
+                    'message': 'User not found'
+                }
+            )
+            serializer.is_valid()
+
+            return Response(serializer.data)
--- bri_works_api/settings.py
+++ bri_works_api/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework.authtoken',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'bri_works_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bri-works',
+        'USER': 'postgres',
+        'PASSWORD': 'hafiz123',
+        'HOST': '127.0.0.1',
+        'PORT': '5433'
     }
 }
 
@@ -99,6 +104,14 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'authentication.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
 
 
 # Internationalization
