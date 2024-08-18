5eb4964956c3082b2e7c51eb07f48c008c13e09a
Achmad Hafiz <achmadhafiz99@gmail.com>
2024-08-19 02:11:27 +0700

    fix: add id to home item responses

--- bri_works_api/views.py
+++ bri_works_api/views.py
@@ -11,24 +11,18 @@ from food.serializers import TenantSerializer
 class HomeAPI(APIView):
   def get(self, request):
     events = Event.objects.order_by('start_time')[:5]
-    # events = Event.objects.all()
     events_serializer = EventSerializer(events, many=True)
-    # events_serializer.is_valid(raise_exception=True) 
     tenants = Tenant.objects.order_by('-rate')[:5]
     tenants_serializer = TenantSerializer(tenants, many=True)
-    # tenants_serializer.is_valid(raise_exception=True)
     spaces = Space.objects.order_by('name')[:5]
     spaces_serializer = SpaceHomeSerializer(spaces, many=True)
-    # spaces_serializer.is_valid(raise_exception=True)
-    print("nyampe")
-    serializer = HomeResponse(
-      data={
-        'message': 'Succesfully retrieved',
-        'data': {
+    
+    response_data = {
+      'message': 'Successfully retrieved',
+      'data': {
           'events': events_serializer.data,
           'tenants': tenants_serializer.data,
           'spaces': spaces_serializer.data
-        }
-    })
-    serializer.is_valid(raise_exception=True)
-    return Response(serializer.data)
+      }
+    }
+    return Response(response_data)
