2a4849425f51528449fb9b74c4386c38ab2f7742
Achmad Hafiz <achmadhafiz99@gmail.com>
2024-08-28 18:01:55 +0700

    feat: add package attributes to current member subscription api

--- member_registration/serializers.py
+++ member_registration/serializers.py
@@ -11,14 +11,16 @@ class MemberRegistrationRequest(serializers.Serializer):
   job = serializers.CharField()
   institution = serializers.CharField(allow_null=True, required=False)
 
-class MemberRegistrationSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = MemberRegistration
-    fields = '__all__'
 
 class MemberPackageSerializer(serializers.ModelSerializer):
   class Meta:
     model = MemberPackage
+    fields = '__all__'
+
+class MemberRegistrationSerializer(serializers.ModelSerializer):
+  package = MemberPackageSerializer()
+  class Meta:
+    model = MemberRegistration
     fields = '__all__'
 
 class UserMemberFormSerializer(serializers.ModelSerializer):
